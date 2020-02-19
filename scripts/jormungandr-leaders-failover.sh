70d543fdd88f1775ef6918c95151d56ec9ae606b
rdlrt <rdlrt@github.com>
2020-02-19 05:14:22 +0000

    jormungandr-leaders-failover: Update output formatting, add timeout for epoch transition, use variable for auto restart

--- scripts/jormungandr-leaders-failover.sh
+++ scripts/jormungandr-leaders-failover.sh
@@ -4,12 +4,13 @@
 # - If you're not sure what the script does, please do not use it directly on the network. Incorrect implementation may cause you to create an adversarial fork and (correctly) report you accordingly on some community sites
 
 # DISCLAIMER:
-# - The script works for a specific 2-node scenario. It is expected that you customise this to your environment and usage (in spite of 'Do not modify' section for basic users).
+# - The script works for a specific 1-pool with 2-nodes scenario. It is expected that you customise this to your environment and usage.
 # - While the actual script is pretty basic in nature, It is assumed that someone using this method is well equipped and qualifies as per the skills and requirements expected of a stakepool operator in the URL below:
 #      https://testnet.iohkdev.io/en/cardano/shelley/about/skills-and-requirements/
 # - The most important outcome is to prevent having multiple nodes exhibiting same key at a time, except for epoch transition. 
 # - We are learning and testing the script (so far the results have been satisfying), please do feel free to add any feedbacks/suggestions to github repo itself
 # - Do not expect any tech support from IOHK Help Desk/official support mediums on the script, it is made by the community - for the community
+# - Be careful when testing against builds that face issues with hung API calls, you could end up with adversarial fork because API didnt give back a reply.
 
 # How nodes are started?
 # - The case below expects that the two leaders are started with their keys during boot up time.
@@ -36,32 +37,49 @@
 #     [Install]
 #     WantedBy=multi-user.target
 
-tput reset
+# How script is run?
+# Ideally you'd want to run the script in a tmux/screen session so that you can resume the output and check the last timestamps for the type of logging based on event
+
+clear
 shopt -s expand_aliases
 
 ##########################
 # Variables to modify
 ##########################
+autorestart="N" # To restart the node that's behind 
 jkey=~/jormu/priv/pool-secret.yaml
-## Parameters for reporting tip to pooltool
-POOL_ID=$(grep node_id $jkey |awk '{print $2}') # assumes you use the YAML2 (default) format for your node keys
 POOLTOOL_UID_FILE=~/jormu/priv/pooltool.uid # Grab this by login to https://pooltool.io/profile
-platformName="jormungandr-leaders-failover.sh"
 GENESIS="8e4d2a343f3dcf9330ad9035b3e8d168e6728904262f2c434a4f8f934ec7b676"
 J1_URL=http://127.0.0.1:4100/api ## Assumes two nodes operating on same host on different ports, change to method as desired
 J2_URL=http://127.0.0.1:4101/api ## It is *NOT* recommended to publish your API endpoint to non trusted client connections
+timeout=300 # Number of iterations before taking action on node that's behind, no reason to reduce this if your node is working fine
 
 ##########################
 # Do not modify below
 ##########################
+
+POOL_ID=$(grep node_id $jkey |awk '{print $2}') # assumes you use the YAML2 (default) format for your node keys
+platformName="jormungandr-leaders-failover.sh"
+jsettingsf="/tmp/.jormu_settings.delme"
+j1statsf="/tmp/.jormu_stats1.delme"
+j2statsf="/tmp/.jormu_stats2.delme"
+pooltoolf="/tmp/.pooltoolresponse.delme"
+jleadf="/tmp/.jlead.delme"
+# Counters
+i=0
+j=1
+
 # Function to set/swap Leader URL vars
+function echom() {
+  echo -en "\033[K\033[$1B\r$(date +%d/%m-%T) - ${@:2}\033[K\033[$1A\r"
+}
+
 setURLvars() {
   J1_URL=$1
   J2_URL=$2
 }
 
 # Collect chain settings
-jsettingsf="/tmp/.jormu_settings.delme"
 jcli rest v0 settings get --output-format json -h $J1_URL > $jsettingsf
 rc=$?
 if [ $rc -ne 0 ]; then
@@ -74,109 +92,129 @@ if [ $rc -ne 0 ]; then
 fi
 slotDuration=$(cat $jsettingsf | jq -r .slotDuration)
 slotsPerEpoch=$(cat $jsettingsf | jq -r .slotsPerEpoch)
+GENESIS=$(jcli rest v0 settings get --output-format json | jq -r .block0Hash)
 jormVersion=$(jcli rest v0 node stats get --output-format json -h $J1_URL | jq -r .version)
 rm -f /tmp/.jormu_settings.delme
-i=0
-timeout=300 # Number of iterations before taking action on node that's behind
 
-j=1
+# Initial screen for echo
+echom 1 "Status: Date: <NA> Height: <NA> Active: $(echo $J1_URL |cut -d/ -f3|cut -d: -f2)"
+echom 2 "Last Sync Difference: <NA>"
+echom 3 "Leader key moved to node at port: $(echo $J1_URL |cut -d/ -f3|cut -d: -f2)"
+echom 4 "Last Pooltool response: <NA>"
 
+# Main Leader Failover loop , note that the test condition will/should never satisfy - if it did, then you modified something incorrectly below (converse is not true)
 while (test "$i" -le $timeout )
 do
-  lBH1=$(jcli rest v0 node stats get --output-format json -h $J1_URL | jq -r .lastBlockHeight)
+  jcli rest v0 node stats get --output-format json -h $J1_URL 2>/dev/null 1>$j1statsf
+  jcli rest v0 node stats get --output-format json -h $J2_URL 2>/dev/null 1>$j2statsf
+  lBH1=$(cat $j1statsf | jq -r .lastBlockHeight)
   lBH2=$(jcli rest v0 node stats get --output-format json -h $J2_URL | jq -r .lastBlockHeight)
-  #currslot=$(echo $((($(date +%s)-1576264417)/$slotsPerEpoch/$slotDuration)).$(((($(date +%s)-1576264417)%($slotsPerEpoch*$slotDuration))/$slotDuration)) | cut -d . -f 2)
+  lBD=$(cat $j1statsf | jq -r .lastBlockDate)
   currslot=$(( (($(date +%s)-1576264417)%($slotsPerEpoch*$slotDuration))/$slotDuration ))
   diffepochend=$(expr $slotsPerEpoch - $currslot)
   if [ -z "${lBH1}" ] || [ "${lBH1}" == "null" ] ;then
-    # RISK: If node stats endpoint was hung , eg: when using an untested release, this could mean you end up loading keys to both nodes!
-    # Expect delete calls to fail, and hence send output of those delete calls to /dev/null, but calls are present to ensure we dont have condition with double load for leaders.
-    echo -e "$(date +%D-%T) - $J1 is unavailable, Attempting to load keys to $J2..."
+    # Expect delete calls to fail, and hence send output of those delete calls to /dev/null.
     jcli rest v0 leaders delete 1 -h $J2_URL 2>&1 >/dev/null
     jcli rest v0 leaders post -f $jkey -h $J2_URL 2>&1 >/dev/null
     jcli rest v0 leaders delete 1 -h $J1_URL 2>&1 >/dev/null
     setURLvars $J2_URL $J1_URL
-    echo -e "$(date +%D-%T) - Node Down, Failover not possible: $i $lBH1 $lBH2 - $diffepochend $J1_URL $J2_URL"
+    echom 1 "Node $(echo $J2_URL |cut -d/ -f3|cut -d: -f2) Down: Height: $lBH2 Active: $(echo $J1_URL |cut -d/ -f3|cut -d: -f2)"
   elif [ -z "${lBH2}" ] || [ "${lBH2}" == "null" ]; then
     sleep $(($slotDuration+1))
-    echo -e "$(date +%D-%T) - Node Down, Failover not possible: $i $lBH1 $lBH2 - $diffepochend $J1_URL $J2_URL"
+    echom 1 "Node $(echo $J2_URL |cut -d/ -f3|cut -d: -f2) Down: Date: $lBD Height: $lBH1 Active: $(echo $J1_URL |cut -d/ -f3|cut -d: -f2)"
   else
     hdiff=$(( $lBH2 - $lBH1 ))
-    lBD=$(jcli rest v0 node stats get --output-format json -h $J1_URL | jq -r .lastBlockDate)
-    # The echo command below is only for troubleshooting while initially setting up, take it out
-    echo -en "\r$(date +%d/%m-%T) - $lBH1 $hdiff $diffepochend $(echo $J1_URL |cut -d/ -f3|cut -d: -f2) $lBD"
-    if [ $diffepochend -lt $(($slotDuration+1)) ]; then # Note: Adds a remote (2/43200) probability of creating an adversarial fork if assigned a leadership slot right at the epoch transition
-      echo -e "$(date +%D-%T) - Adding keys to both nodes for epoch transition"
-      # Based on this script J1 is active and will always have the leader key, so add to J2
+    echom 1 "Status: Date: $lBD Height: $lBH1 Diff: $hdiff Active: $(echo $J1_URL |cut -d/ -f3|cut -d: -f2)"
+    # Epoch Transition: Remote (2/43200) probability of creating an adversarial fork if assigned a leadership slot right at the epoch transition
+    if [ $diffepochend -lt $(($slotDuration+1)) ]; then
+      echom 5 "Last Epoch Transition Status:"
+      echom 6 " - Entered: Adding keys, pausing failover capabilities.."
+      # Based on this script J1 is active and will always have the leader key, so only add to J2
       jcli rest v0 leaders post -f $jkey -h $J2_URL > /dev/null
       sleep $(($slotDuration+1))
       J2LEADSLOTCNT=0
       epochtranscnt=0
       while(test "$J2LEADSLOTCNT" -eq 0)
+      # Loop waiting for leadership scheduler to fill atleast one slot to new epoch - else use $timeout to proceed
       do
-        sleep $(($slotDuration/2))
-        echo -en "\r$(date +%d/%m-%T) - Waiting for node at $J2_URL to have a pending slot in current epoch $((epochtranscnt++)) out of $timeout times.."
         if [ $epochtranscnt -gt $timeout ]; then
-          echo -e "$(date +%D-%T) - Timeout reached waiting for leader logs at epoch transition, resuming failover capabilities.."
-          J2LEADSLOTCNT=-1 #quit the loop because of timeout
+          J2LEADSLOTCNT=-1
+          echom 8 " - Complete: No slots scheduled for this epoch (timeout reached)"
         else
+          echom 7 " - Processing: Waiting for a leadership slot for node $J2_URL - ($((epochtranscnt++))/$timeout)  time(s).."
+          sleep $(($slotDuration/2))
           J2LEADSLOTCNT=$(jcli rest v0 leaders logs get -h $J2_URL | grep "wake_at_time: ~"  | wc -l)
+          if [ $J2LEADSLOTCNT -gt 0 ]; then
+            jcli rest v0 leaders logs get | grep -e scheduled | awk 'NR%2{printf "%s ",$0;next;}1' | sed -e s/scheduled_at_//g  | sort -V | column -t > $jleadf
+            echom 8 " - Complete: Schedule loaded successfully"
+          fi
         fi
       done
       jcli rest v0 leaders delete 1 -h $J2_URL > /dev/null
-    fi
-    # Change if appropriate to your case: The extract below assumes your leaders will only have 1 key, and that leader ID 1(referred later for delete) points to the pool used for failover
-    # Ensure J1 only has 1 leader ID, delete others
-    loopchk=1
-    while [ "$loopchk" -eq 1 ]; do
-      J1LEADCNT=$(jcli rest v0 leaders get -h $J1_URL | tail -1 | awk '{print $2}')
-      loopchk=0
-      if [ ! -z "$J1LEADCNT" ]; then
-        if [ $J1LEADCNT -gt 1 ]; then
-          jcli rest v0 leaders delete $J1LEADCNT -h $J1_URL > /dev/null
-          loopchk=1
-        fi
-      else
-        jcli rest v0 leaders post -f $jkey -h $J1_URL > /dev/null
-      fi
-    done
-
-    # As per this script only J1 should be a leader, if J2 has leader keys, delete them
-    loopchk=1
-    while [ "$loopchk" -eq 1 ]; do
-      J2LEADCNT=$(jcli rest v0 leaders get -h $J2_URL | tail -1 | awk '{print $2}')
-      if [ ! -z "$J2LEADCNT" ]; then
-        jcli rest v0 leaders delete 1 -h $J2_URL > /dev/null
-      else
+    else
+      # If not at epoch transition, make sure there is only 1 leader at a time
+      # Ensure J1 has only 1 leader ID, delete others
+      loopchk=1
+      while [ "$loopchk" -eq 1 ]; do
+        J1LEADCNT=$(jcli rest v0 leaders get -h $J1_URL | tail -1 | awk '{print $2}')
         loopchk=0
-      fi
-    done
+        if [ ! -z "$J1LEADCNT" ]; then
+          if [ $J1LEADCNT -gt 1 ]; then
+            jcli rest v0 leaders delete $J1LEADCNT -h $J1_URL > /dev/null
+            loopchk=1
+          fi
+        else
+          jcli rest v0 leaders post -f $jkey -h $J1_URL > /dev/null
+        fi
+      done
+
+      # Ensure J2 has no leader keys
+      loopchk=1
+      while [ "$loopchk" -eq 1 ]; do
+        J2LEADCNT=$(jcli rest v0 leaders get -h $J2_URL | tail -1 | awk '{print $2}')
+        if [ ! -z "$J2LEADCNT" ]; then
+          jcli rest v0 leaders delete 1 -h $J2_URL > /dev/null
+        else
+          loopchk=0
+        fi
+      done
+    fi
     sleep $(($slotDuration/2))
     if [ "$hdiff" -gt 0 ]; then
-      echo -e "\n$(date +%D-%T) - J1 found to be behind J2 $((i++ + 1)) times"
-      if [ "$i" -ge 2 ]; then # if J2 is ahead for consecutive 1.5 slots, swap leadership for 2nd slot
-        echo -e "$(date +%D-%T) - Swapping keys..."
+      echom 2 "Last Sync Difference: $(echo $J1_URL |cut -d/ -f3|cut -d: -f2) was behind $(echo $J2_URL |cut -d/ -f3|cut -d: -f2) $((i++ + 1)) time(s)"
+      # if block height of J1 is behind for consecutive 1.5 slots, swap leadership for 2nd slot
+      if [ "$i" -ge 2 ]; then
+        echom 3 "Leader key moved to node at port: $(echo $J2_URL |cut -d/ -f3|cut -d: -f2)"
         jcli rest v0 leaders post -f $jkey -h $J2_URL > /dev/null
         jcli rest v0 leaders delete 1 -h $J1_URL > /dev/null
-		setURLvars $J2_URL $J1_URL
-		i=0
+		    setURLvars $J2_URL $J1_URL
+		    i=0
       fi
     elif [ "$hdiff" -lt -5 ]; then
-      echo -e "\n$(date +%D-%T) - J2 found to be behind J1 $((i++ + 1)) times"
+      # if blockheight of J2 is behind by more than 5 blocks for $timeout itertions take action if auto restart is set to yes.
+      echom 2 "Last Sync Difference: $(echo $J2_URL |cut -d/ -f3|cut -d: -f2) was behind $(echo $J1_URL |cut -d/ -f3|cut -d: -f2) $((i++ + 1)) time(s)"
       if [ "$i" -ge $timeout ]; then
-        # Comment the line below if you do not experience hang of nodes.
-        # Restarting the node is not a good solution for the network, and if used - should only be a temporary remediation. Starting from 0.8.6, the node is able to catch up fine without restart
-        jcli rest v0 shutdown get -h $J2_URL
-        echo -e "$(date +%D-%T) - J2 has been stuck; Resetting due to timeout..\n" >> /tmp/killjormu.log >&2
+        if [ "${autorestart}" == "N" ]; then
+          jcli rest v0 shutdown get -h $J2_URL
+          echom 8 "Last Node Reset due to timeout: $(echo $J2_URL |cut -d/ -f3|cut -d: -f2)" >> /tmp/killjormu.log >&2
+        fi
         i=0
       fi
     else
       i=0
     fi
   fi
+  # Report to Pooltool once in 14.5 * slotDuration seconds
   if [ $(( j++ + 1)) -gt 15 ]; then
-    curl -s -G --data-urlencode "platform=$platformName" --data-urlencode "jormver=$jormVersion" "https://api.pooltool.io/v0/sharemytip?poolid=${POOL_ID}&userid=$(cat $POOLTOOL_UID_FILE)&genesispref=${GENESIS}&mytip=${lBH1}" > /dev/null
-	j=1
+    lastBlockHash=$(cat $j1statsf | jq -r .lastBlockHash)
+    lastBlock=$(jcli rest v0 block ${lastBlockHash} get -h $J1_URL 2>/dev/null)
+    lastPoolID=${lastBlock:168:64}
+    lastParent=${lastBlock:104:64}
+    lastSlot=$((0x${lastBlock:24:8}))
+    lastEpoch=$((0x${lastBlock:16:8}))
+    curl -s -G --data-urlencode "platform=$platformName" --data-urlencode "jormver=$jormVersion" "https://api.pooltool.io/v0/sharemytip?poolid=${POOL_ID}&userid=$(cat $POOLTOOL_UID_FILE)&genesispref=${GENESIS}&mytip=${lBH1}&lasthash=${lastBlockHash}&lastpool=${lastPoolID}&lastparent=${lastParent}&lastslot=${lastSlot}&lastepoch=${lastEpoch}" 2>/dev/null > $pooltoolf
+    echom 4 "Last Pooltool response: Success: $(cat $pooltoolf | jq -r .success) MaxHeight: $(cat $pooltoolf | jq -r .pooltoolmax)"
+	  j=1
   fi
   sleep $(($slotDuration/2))
 done
